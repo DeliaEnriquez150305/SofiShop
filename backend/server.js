3f9eb0cb1905dcbaae3fd1d32973a3d0fed2c73a
DeliaEnriquez150305 <deliaenriquez150305@gmail.com>
2026-02-08 14:28:56 -0500

    Coreccion url imagenes

--- backend/server.js
+++ backend/server.js
@@ -416,8 +416,8 @@ app.use(express.static(path.join(__dirname, '../frontend')));
 app.use('/uploads', express.static(path.join(__dirname, '../frontend/uploads')));
 
 // Servir carpetas de perfumes (con espacios en el nombre)
-app.use('/perfumes%20mujer', express.static(path.join(__dirname, '../frontend/perfumes mujer')));
-app.use('/perfumes%20hombre', express.static(path.join(__dirname, '../frontend/perfumes hombre')));
+app.use('/perfumes-mujer', express.static(path.join(__dirname, '../frontend/perfumes mujer')));
+app.use('/perfumes-hombre', express.static(path.join(__dirname, '../frontend/perfumes hombre')));
 
 // ============================================
 // RUTA RAÍZ
