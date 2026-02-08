5d2b9d804a7668f91b4790a92ddd93ddaa8213e6
DeliaEnriquez150305 <deliaenriquez150305@gmail.com>
2026-02-08 13:30:03 -0500

    Cambio correccion base

--- frontend/admin-pedidos-fixed.js
+++ frontend/admin-pedidos-fixed.js
@@ -1,7 +1,7 @@
 // Script temporal para probar la carga desde backend
 async function obtenerPedidosBackend() {
   try {
-    const response = await fetch('http://localhost:3000/api/orders');
+    const response = await fetch('mongodb+srv://deliaenriquez_db_user:admin123@sofi.5a5opzn.mongodb.net/');
     if (!response.ok) throw new Error('Error al obtener pedidos');
     const pedidos = await response.json();
     console.log('Pedidos del backend:', pedidos);
